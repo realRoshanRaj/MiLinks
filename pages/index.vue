d71e04f746473b87b37661fa86e2bce48293c557
Roshan Raj <roshan.raj@frogforce503.org>
2020-08-22 21:08:30 -0400

    Add minimal landing page

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,24 @@
   <v-container class="fill-height" fluid>
     <v-row align="center" justify="center">
       <v-col cols="12" md="6" sm="7">
-        <h1 class="mb-2 display-1 text-center" style="font-weight: 900;">
-          The social network for epic content</h1>
-        <div class="subheading mb-3 text-center" justify-center
-             style="font-weight: 900">Unlesh your creativity without limitations
-        </div>
+        <div class="text-h4 mb-2 text-center text-no-wrap">
+          One link for everything your bio needs</div>
+        <p class="text-subheading font-weight-bold mb-3 text-center"
+          >Every link you need, all in one place
+        </p>
 
         <v-layout justify-center>
-          <v-btn class="mt-5" color="mainPurple" rounded large to="/register">
+          <v-btn class="my-5" color="mainPurple" rounded large to="/register">
             Sign up now!
           </v-btn>
         </v-layout>
+
+        <p class="text-center">
+          <nuxt-link
+            to="/Login"
+            style="text-decoration: none; color: inherit;"
+          >Already signed up? Login</nuxt-link>
+        </p>
       </v-col>
     </v-row>
   </v-container>
